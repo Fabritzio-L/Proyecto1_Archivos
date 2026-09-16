225a6f592b89abc5d6ae6419db1d88b0d33a1c23
fabri <fabritzioalejandrol@gmail.com>
2026-09-15 22:51:49 -0600

    metodos para cargar y guardar la configuracion

--- gestor_config.py
+++ gestor_config.py
@@ -18,3 +18,45 @@ class GestorConfiguracion:
             "ruta_foto": ""
         }
 
+    def cargar_configuracion(self):
+        try:
+            with open(self.archivo_final, "r", encoding="utf-8") as archivo:
+                configuracion = json.load(archivo)
+                print("Configuración cargada exitosamente")
+                return configuracion
+                
+        except FileNotFoundError:
+            print("Aviso: Archivo de configuración ausente. Cargando valores por defecto.")
+            return self.configuracion_por_defecto.copy()
+            
+        except Exception as e:
+            print(f"Error al leer: {e}. Cargando valores por defecto.")
+            return self.configuracion_por_defecto.copy()
+
+    def guardar_configuracion(self, nuevos_datos):
+        try:
+            with open(self.archivo_temporal, "w", encoding="utf-8") as archivo:
+                json.dump(nuevos_datos,
+                        archivo,
+                        indent=4, 
+                        ensure_ascii=False)
+            
+            if os.path.exists(self.archivo_final):
+                os.replace(self.archivo_final, self.archivo_respaldo)
+                
+            os.replace(self.archivo_temporal, self.archivo_final)
+            print("Configuración guardada de manera segura")
+            return True
+            
+        except PermissionError:
+            print("Error: Falta de permisos de escritura. No se pudo guardar la configuración")
+            if os.path.exists(self.archivo_temporal):
+                try:
+                    os.remove(self.archivo_temporal)
+                except:
+                    pass
+            return False
+            
+        except Exception as e:
+            print(f"Error inesperado al guardar {e}")
+            return False
